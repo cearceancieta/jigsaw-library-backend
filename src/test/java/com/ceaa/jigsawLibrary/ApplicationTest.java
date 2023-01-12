3a98fa19351a3e5b58f10429d7bb35eb10df3ab3
Cristian Arce <cearceancieta@gmail.com>
2023-01-12 12:28:43 -0500

    Adding spring-boot dependencies

---
+++ src/test/java/com/ceaa/jigsawLibrary/ApplicationTest.java
@@ -0,0 +1,13 @@
+package com.ceaa.jigsawLibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTest {
+
+    @Test
+    void contextLoad() {
+
+    }
+}
