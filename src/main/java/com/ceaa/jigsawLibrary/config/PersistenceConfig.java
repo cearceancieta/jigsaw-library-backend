44e968820c2a09e2d6c1cf5954561312b5cf40f4
Cristian Arce <cearceancieta@gmail.com>
2023-02-17 22:43:23 -0500

    Adding persistence with H2 db

---
+++ src/main/java/com/ceaa/jigsawLibrary/config/PersistenceConfig.java
@@ -0,0 +1,7 @@
+package com.ceaa.jigsawLibrary.config;
+
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@EnableJpaRepositories(basePackages = "com.ceaa.jigsawLibrary.repositories")
+public class PersistenceConfig {
+}
